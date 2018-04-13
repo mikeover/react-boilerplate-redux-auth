f068d956e1726b30d7ef0cbfdd61a21c2a27386c
Mike Over <mike.over@teamsnap.com>
2018-04-13 11:22:01 -0400

    Get redux-auth working with my forked repo, and material-ui

--- app/containers/FeaturePage/index.js
+++ app/containers/FeaturePage/index.js
@@ -12,6 +12,7 @@ import messages from './messages';
 import List from './List';
 import ListItem from './ListItem';
 import ListItemTitle from './ListItemTitle';
+import { EmailSignUpForm } from "redux-auth/material-ui-theme";
 
 export default class FeaturePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -31,6 +32,7 @@ export default class FeaturePage extends React.Component { // eslint-disable-lin
         <H1>
           <FormattedMessage {...messages.header} />
         </H1>
+        <EmailSignUpForm />
         <List>
           <ListItem>
             <ListItemTitle>
